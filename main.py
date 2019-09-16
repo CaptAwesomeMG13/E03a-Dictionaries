c712b58dc76ad1a340c7b50aa4d0f8d06ece1e91
Mitch <mggilchrist@att.net>
2019-09-16 16:47:54 -0400

    dictionary made

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 
+bdays = {
+    "Mitch": "7/13/99",
+    "Jake": "3/21/99",
+    "Sean": "6/28/99",
+    "Cailin": "7/2/99",
+    "John": "11/30/98",
+    "Stacy": "1/19/98",
+    "Max": "5/25/99"
+}
+
+print(bdays.keys())
+ans = input("Whose birthday would you like to see?  ")
+print(bdays[ans])
